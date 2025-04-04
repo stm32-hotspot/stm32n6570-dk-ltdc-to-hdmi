fa742cade083b115a7505083fbc7665eb3b05cf1
Guillaume Legrain <guillaume.legrain@st.com>
2025-04-04 13:09:56 +0200

    Initial commit

---
+++ Src/main.c
@@ -0,0 +1,353 @@
+ /**
+ ******************************************************************************
+ * @file    main.c
+ * @author  GPM Application Team
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+#include "cmw_camera.h"
+#include "stm32n6570_discovery_bus.h"
+#include "stm32n6570_discovery_lcd.h"
+#include "stm32n6570_discovery_xspi.h"
+#include "stm32n6570_discovery.h"
+#include "stm32_lcd.h"
+#include "stm32_lcd_ex.h"
+#include "main.h"
+#include <stdio.h>
+#include <assert.h>
+
+#define LCD_BG_WIDTH  800U
+#define LCD_BG_HEIGHT 480U
+
+#define LCD_FG_WIDTH             320U
+#define LCD_FG_HEIGHT             50U
+#define LCD_FG_FRAMEBUFFER_SIZE  (LCD_FG_WIDTH * LCD_FG_HEIGHT * 2)
+
+typedef struct
+{
+  uint32_t X0;
+  uint32_t Y0;
+  uint32_t XSize;
+  uint32_t YSize;
+} Rectangle_TypeDef;
+
+/* Lcd Background area */
+Rectangle_TypeDef lcd_bg_area = {
+  .X0 = 0,
+  .Y0 = 0,
+  .XSize = LCD_BG_WIDTH,
+  .YSize = LCD_BG_HEIGHT,
+};
+
+/* Lcd Foreground area */
+Rectangle_TypeDef lcd_fg_area = {
+  .X0 = 0,
+  .Y0 = 0,
+  .XSize = LCD_FG_WIDTH,
+  .YSize = LCD_FG_HEIGHT,
+};
+
+/* Lcd Background Buffer */
+__attribute__ ((section (".psram_bss")))
+__attribute__ ((aligned (32)))
+uint8_t lcd_bg_buffer[LCD_BG_WIDTH * LCD_BG_HEIGHT * 2];
+
+/* Lcd Foreground Buffer */
+__attribute__ ((section (".psram_bss")))
+__attribute__ ((aligned (32)))
+uint8_t lcd_fg_buffer[LCD_FG_WIDTH * LCD_FG_HEIGHT * 2];
+
+static void SystemClock_Config(void);
+static void Hardware_init(void);
+static void Camera_Init(void);
+static void LCD_init(void);
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+
+  Hardware_init();
+
+  Camera_Init();
+
+  LCD_init();
+
+  UTIL_LCD_SetLayer(LTDC_LAYER_2);
+  UTIL_LCD_Clear(0x00000000UL);
+  UTIL_LCD_SetFont(&Font20);
+  UTIL_LCD_SetTextColor(UTIL_LCD_COLOR_WHITE);
+
+  UTIL_LCD_FillRect(0, 0, LCD_FG_WIDTH, LINE(2), 0x80202020UL); /* dark gray 50% opacity */
+  UTIL_LCD_SetBackColor(0x80202020UL); /* dark gray 50% opacity */
+
+  UTIL_LCDEx_PrintfAtLine(1, "%dx%d", LCD_BG_WIDTH, LCD_BG_HEIGHT);
+
+  int32_t ret = CMW_CAMERA_Start(DCMIPP_PIPE1, lcd_bg_buffer, CMW_MODE_CONTINUOUS);
+  assert(ret == CMW_ERROR_NONE);
+
+  /*** App Loop ***************************************************************/
+  while (1)
+  {
+    ret = CMW_CAMERA_Run(); /* Update ISP */
+    assert(ret == CMW_ERROR_NONE);
+  }
+}
+
+static void Hardware_init(void)
+{
+  /* Power on ICACHE */
+  MEMSYSCTL->MSCR |= MEMSYSCTL_MSCR_ICACTIVE_Msk;
+
+  /* Set back system and CPU clock source to HSI */
+  __HAL_RCC_CPUCLK_CONFIG(RCC_CPUCLKSOURCE_HSI);
+  __HAL_RCC_SYSCLK_CONFIG(RCC_SYSCLKSOURCE_HSI);
+
+  HAL_Init();
+
+  SCB_EnableICache();
+  /* Power on DCACHE */
+  MEMSYSCTL->MSCR |= MEMSYSCTL_MSCR_DCACTIVE_Msk;
+  SCB_EnableDCache();
+
+  SystemClock_Config();
+
+  BSP_XSPI_RAM_Init(0);
+  BSP_XSPI_RAM_EnableMemoryMappedMode(0);
+
+  /* Set all required IPs as secure privileged */
+  __HAL_RCC_RIFSC_CLK_ENABLE();
+  RIMC_MasterConfig_t RIMC_master = {0};
+  RIMC_master.MasterCID = RIF_CID_1;
+  RIMC_master.SecPriv = RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV;
+  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_DMA2D, &RIMC_master);
+  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_DCMIPP, &RIMC_master);
+  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_LTDC1 , &RIMC_master);
+  HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_LTDC2 , &RIMC_master);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_DMA2D , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_CSI    , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_DCMIPP , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_LTDC   , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_LTDCL1 , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+  HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_LTDCL2 , RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV);
+}
+
+static void Camera_Init(void)
+{
+  CMW_CameraInit_t cam_conf;
+  CMW_DCMIPP_Conf_t dcmipp_conf;
+  uint32_t pitch;
+  int32_t ret;
+
+  cam_conf.width = 0; /* Leave the driver use the default resolution */
+  cam_conf.height = 0; /* Leave the driver use the default resolution */
+  cam_conf.fps = 30;
+  cam_conf.pixel_format = 0; /* Default; Not implemented yet */
+  cam_conf.anti_flicker = 0;
+  cam_conf.mirror_flip = CMW_MIRRORFLIP_NONE;
+  ret = CMW_CAMERA_Init(&cam_conf);
+  assert(ret == CMW_ERROR_NONE);
+
+  dcmipp_conf.output_width = LCD_BG_WIDTH;
+  dcmipp_conf.output_height = LCD_BG_HEIGHT;
+  dcmipp_conf.output_format = DCMIPP_PIXEL_PACKER_FORMAT_RGB565_1;
+  dcmipp_conf.output_bpp = 2;
+  dcmipp_conf.mode = CMW_Aspect_ratio_crop;
+  dcmipp_conf.enable_swap = 0;
+  dcmipp_conf.enable_gamma_conversion = 0;
+  ret = CMW_CAMERA_SetPipeConfig(DCMIPP_PIPE1, &dcmipp_conf, &pitch);
+  assert(ret == HAL_OK);
+  assert(dcmipp_conf.output_width * dcmipp_conf.output_bpp == pitch);
+}
+
+static void LCD_init(void)
+{
+  BSP_LCD_LayerConfig_t LayerConfig = {0};
+
+  BSP_LCD_Init(0, LCD_ORIENTATION_LANDSCAPE);
+
+  /* Preview layer Init */
+  LayerConfig.X0          = lcd_bg_area.X0;
+  LayerConfig.Y0          = lcd_bg_area.Y0;
+  LayerConfig.X1          = lcd_bg_area.X0 + lcd_bg_area.XSize;
+  LayerConfig.Y1          = lcd_bg_area.Y0 + lcd_bg_area.YSize;
+  LayerConfig.PixelFormat = LCD_PIXEL_FORMAT_RGB565;
+  LayerConfig.Address     = (uint32_t) lcd_bg_buffer;
+  BSP_LCD_ConfigLayer(0, LTDC_LAYER_1, &LayerConfig);
+
+  LayerConfig.X0 = lcd_fg_area.X0;
+  LayerConfig.Y0 = lcd_fg_area.Y0;
+  LayerConfig.X1 = lcd_fg_area.X0 + lcd_fg_area.XSize;
+  LayerConfig.Y1 = lcd_fg_area.Y0 + lcd_fg_area.YSize;
+  LayerConfig.PixelFormat = LCD_PIXEL_FORMAT_ARGB4444;
+  LayerConfig.Address = (uint32_t) lcd_fg_buffer;
+  BSP_LCD_ConfigLayer(0, LTDC_LAYER_2, &LayerConfig);
+
+  UTIL_LCD_SetFuncDriver(&LCD_Driver);
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef RCC_PeriphCLKInitStruct = {0};
+
+  /* Ensure VDDCORE=0.9V before increasing the system frequency */
+  BSP_SMPS_Init(SMPS_VOLTAGE_OVERDRIVE);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_NONE;
+
+  /* PLL1 = 64 x 25 / 2 = 800MHz */
+  RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL1.PLLM = 2;
+  RCC_OscInitStruct.PLL1.PLLN = 25;
+  RCC_OscInitStruct.PLL1.PLLFractional = 0;
+  RCC_OscInitStruct.PLL1.PLLP1 = 1;
+  RCC_OscInitStruct.PLL1.PLLP2 = 1;
+
+  /* PLL2 = 64 x 125 / 8 = 1000MHz */
+  RCC_OscInitStruct.PLL2.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL2.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL2.PLLM = 8;
+  RCC_OscInitStruct.PLL2.PLLFractional = 0;
+  RCC_OscInitStruct.PLL2.PLLN = 125;
+  RCC_OscInitStruct.PLL2.PLLP1 = 1;
+  RCC_OscInitStruct.PLL2.PLLP2 = 1;
+
+  /* PLL3 = (64 x 225 / 8) / (1 * 2) = 900MHz */
+  RCC_OscInitStruct.PLL3.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL3.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL3.PLLM = 8;
+  RCC_OscInitStruct.PLL3.PLLN = 225;
+  RCC_OscInitStruct.PLL3.PLLFractional = 0;
+  RCC_OscInitStruct.PLL3.PLLP1 = 1;
+  RCC_OscInitStruct.PLL3.PLLP2 = 2;
+
+  /* PLL4 = (64 x 225 / 8) / (6 * 6) = 50 MHz */
+  RCC_OscInitStruct.PLL4.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL4.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL4.PLLM = 8;
+  RCC_OscInitStruct.PLL4.PLLFractional = 0;
+  RCC_OscInitStruct.PLL4.PLLN = 225;
+  RCC_OscInitStruct.PLL4.PLLP1 = 6;
+  RCC_OscInitStruct.PLL4.PLLP2 = 6;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    while(1);
+  }
+
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_CPUCLK | RCC_CLOCKTYPE_SYSCLK |
+                                 RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 |
+                                 RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK4 |
+                                 RCC_CLOCKTYPE_PCLK5);
+
+  /* CPU CLock (sysa_ck) = ic1_ck = PLL1 output/ic1_divider = 800 MHz */
+  RCC_ClkInitStruct.CPUCLKSource = RCC_CPUCLKSOURCE_IC1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_IC2_IC6_IC11;
+  RCC_ClkInitStruct.IC1Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC1Selection.ClockDivider = 1;
+
+  /* AXI Clock (sysb_ck) = ic2_ck = PLL1 output/ic2_divider = 400 MHz */
+  RCC_ClkInitStruct.IC2Selection.ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_ClkInitStruct.IC2Selection.ClockDivider = 2;
+
+  /* NPU Clock (sysc_ck) = ic6_ck = PLL2 output/ic6_divider = 1000 MHz */
+  RCC_ClkInitStruct.IC6Selection.ClockSelection = RCC_ICCLKSOURCE_PLL2;
+  RCC_ClkInitStruct.IC6Selection.ClockDivider = 1;
+
+  /* AXISRAM3/4/5/6 Clock (sysd_ck) = ic11_ck = PLL3 output/ic11_divider = 900 MHz */
+  RCC_ClkInitStruct.IC11Selection.ClockSelection = RCC_ICCLKSOURCE_PLL3;
+  RCC_ClkInitStruct.IC11Selection.ClockDivider = 1;
+
+  /* HCLK = sysb_ck / HCLK divider = 200 MHz */
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+
+  /* PCLKx = HCLK / PCLKx divider = 200 MHz */
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+  RCC_ClkInitStruct.APB5CLKDivider = RCC_APB5_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct) != HAL_OK)
+  {
+    while (1);
+  }
+
+  RCC_PeriphCLKInitStruct.PeriphClockSelection = 0;
+
+  /* XSPI1 kernel clock (ck_ker_xspi1) = HCLK = 200MHz */
+  RCC_PeriphCLKInitStruct.PeriphClockSelection |= RCC_PERIPHCLK_XSPI1;
+  RCC_PeriphCLKInitStruct.Xspi1ClockSelection = RCC_XSPI1CLKSOURCE_HCLK;
+
+  /* XSPI2 kernel clock (ck_ker_xspi1) = HCLK =  200MHz */
+  RCC_PeriphCLKInitStruct.PeriphClockSelection |= RCC_PERIPHCLK_XSPI2;
+  RCC_PeriphCLKInitStruct.Xspi2ClockSelection = RCC_XSPI2CLKSOURCE_HCLK;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphCLKInitStruct) != HAL_OK)
+  {
+    while (1);
+  }
+}
+
+HAL_StatusTypeDef MX_DCMIPP_ClockConfig(DCMIPP_HandleTypeDef *hdcmipp)
+{
+  RCC_PeriphCLKInitTypeDef RCC_PeriphCLKInitStruct = {0};
+  HAL_StatusTypeDef ret = HAL_OK;
+
+  RCC_PeriphCLKInitStruct.PeriphClockSelection = RCC_PERIPHCLK_DCMIPP;
+  RCC_PeriphCLKInitStruct.DcmippClockSelection = RCC_DCMIPPCLKSOURCE_IC17;
+  RCC_PeriphCLKInitStruct.ICSelection[RCC_IC17].ClockSelection = RCC_ICCLKSOURCE_PLL2;
+  RCC_PeriphCLKInitStruct.ICSelection[RCC_IC17].ClockDivider = 3;
+  ret = HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphCLKInitStruct);
+  if (ret != HAL_OK)
+  {
+    return ret;
+  }
+
+  RCC_PeriphCLKInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CSI;
+  RCC_PeriphCLKInitStruct.ICSelection[RCC_IC18].ClockSelection = RCC_ICCLKSOURCE_PLL1;
+  RCC_PeriphCLKInitStruct.ICSelection[RCC_IC18].ClockDivider = 40;
+  ret = HAL_RCCEx_PeriphCLKConfig(&RCC_PeriphCLKInitStruct);
+  if (ret != HAL_OK)
+  {
+    return ret;
+  }
+
+  return ret;
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  UNUSED(file);
+  UNUSED(line);
+  __BKPT(0);
+  while (1)
+  {
+  }
+}
+
+#endif
